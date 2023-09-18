2ad09e657ac470193d30dcaeae8d5c66e399f807
Tom Virgo <t.virgo@iwoca.co.uk>
2023-09-18 18:32:30 +0100

    add more endpoints

--- fpl/client.py
+++ fpl/client.py
@@ -1,5 +1,5 @@
-import requests
 from typing import List
+import requests
 
 BASE_URL = "https://fantasy.premierleague.com/api"
 
@@ -7,15 +7,35 @@ BASE_URL = "https://fantasy.premierleague.com/api"
 class FPLClient:
     def __init__(self, manager_id: int) -> None:
         self.manager_id = manager_id
-        self.data = {}
+        self.overview_url = f"{BASE_URL}/bootstrap-static/"
+        overview_json = requests.get(self.overview_url).json()
+        self.events = overview_json.get("events", [])
+        self.data = {self.overview_url: overview_json}
 
     def get_urls(self) -> List[str]:
-        overview = f"{BASE_URL}/bootstrap-static/"
         manager_summary = f"{BASE_URL}/entry/{self.manager_id}/"
         manager_history = f"{BASE_URL}/entry/{self.manager_id}/history/"
         manager_transfers = f"{BASE_URL}/entry/{self.manager_id}/transfers/"
 
-        return [overview, manager_summary, manager_history, manager_transfers]
+        latest_finished_gw = 1
+        for event in self.events:
+            finished = event["finished"]
+            if finished and event["id"] > latest_finished_gw:
+                latest_finished_gw = event["id"]
+
+        manager_picks_urls = []
+        gw_info_urls = []
+        for gw in range(1, latest_finished_gw + 1):
+            manager_picks_urls.append(
+                f"{BASE_URL}/entry/{self.manager_id}/event/{gw}/picks/"
+            )
+            gw_info_urls.append(f"{BASE_URL}/event/{gw}/live/")
+
+        return (
+            manager_picks_urls
+            + gw_info_urls
+            + [manager_summary, manager_history, manager_transfers]
+        )
 
     def retrieve_data_from_endpoints(self):
         for url in self.get_urls():
