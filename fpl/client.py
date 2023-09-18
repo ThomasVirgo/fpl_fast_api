a4440bbe1cebff079038a790e8b9face3231fe67
Tom Virgo <t.virgo@iwoca.co.uk>
2023-09-18 18:06:43 +0100

    more endpoints

--- fpl/client.py
+++ fpl/client.py
@@ -3,8 +3,6 @@ from typing import List
 
 BASE_URL = "https://fantasy.premierleague.com/api"
 
-endpoints = {"overview": f"{BASE_URL}/bootstrap-static/"}
-
 
 class FPLClient:
     def __init__(self, manager_id: int) -> None:
@@ -12,8 +10,12 @@ class FPLClient:
         self.data = {}
 
     def get_urls(self) -> List[str]:
-        overview_url = endpoints["overview"]
-        return [overview_url]
+        overview = f"{BASE_URL}/bootstrap-static/"
+        manager_summary = f"{BASE_URL}/entry/{self.manager_id}/"
+        manager_history = f"{BASE_URL}/entry/{self.manager_id}/history/"
+        manager_transfers = f"{BASE_URL}/entry/{self.manager_id}/transfers/"
+
+        return [overview, manager_summary, manager_history, manager_transfers]
 
     def retrieve_data_from_endpoints(self):
         for url in self.get_urls():
