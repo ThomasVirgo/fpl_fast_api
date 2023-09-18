a78058f726e04c9d18060ce339c285b0e0414ef7
Tom Virgo <t.virgo@iwoca.co.uk>
2023-09-18 17:54:08 +0100

    update endpoints

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 from fpl.client import FPLClient
@@ -9,15 +7,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "welcome to fpl wrapped"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get("/items/{manager_id}")
-def read_item(manager_id: int):
+@app.get("/fpl/{manager_id}")
+def fpl(manager_id: int):
     fpl_client = FPLClient(manager_id)
     return fpl_client.retrieve_data_from_endpoints()
